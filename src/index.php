ee86b855918eaad9b1f9a6607c946261a2fbebc8
Diego <diegocubilla2001@gmail.com>
2024-04-27 10:48:20 -0400

    base

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../output.css" rel="stylesheet">
+    <title>SERVI365</title>
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        HOLA MUNDO
+    </h1>
+</body>
+
+</html>
